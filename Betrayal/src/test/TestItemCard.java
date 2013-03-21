0fb5460b9e76a58c85f799563417328b14589b5e
rodriga <rodriga@rose-hulman.edu>
2013-03-21 12:31:23 -0400

    Added test to test puzzle box card initilization

--- Betrayal/src/test/TestItemCard.java
+++ Betrayal/src/test/TestItemCard.java
@@ -13,6 +13,8 @@ public class TestItemCard {
 			"A perfect feather fluttering in your hand.");
 	private ItemCard adrenalineShotCard = new AdrenalineShot("Adrenaline Shot", "A syringe containing a strange fluorescent liquid.");
 	private ItemCard revolverCard = new Revolver("Revolver","WEAPON An old, potent-looking weapon.");	
+	private ItemCard puzzleBoxCard = new PuzzleBox("Puzzle Box", "There must be a way to open it.");
+	
 	@Test
 	public void TestAngelFeatherInit() {
 		assertEquals("Angel Feather", angelFeatherCard.getName());
@@ -30,5 +32,11 @@ public class TestItemCard {
 	public void TestRevolverInit(){
 		assertEquals("Revolver", revolverCard.getName());
 		assertEquals("WEAPON An old, potent-looking weapon.", revolverCard.getDescription());
+	}
+	
+	@Test
+	public void TestPuzzleBoxInit(){
+		assertEquals("Puzzle Box", puzzleBoxCard.getName());
+		assertEquals("There must be a way to open it.", puzzleBoxCard.getDescription());
 	}
 }
