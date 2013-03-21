7cd4f4fa5a938b7cf9d1d5bde59b6becf075e5a4
rodriga <rodriga@rose-hulman.edu>
2013-03-20 22:08:51 -0400

    added an OmenCard object card that will be tested

--- Betrayal/src/test/TestOmenCard.java
+++ Betrayal/src/test/TestOmenCard.java
@@ -2,9 +2,13 @@ package test;
 
 import static org.junit.Assert.*;
 
+import omenCards.OmenCard;
+
 import org.junit.Test;
 
 public class TestOmenCard {
+	
+	private OmenCard card;
 
 	@Test
 	public void test() {
