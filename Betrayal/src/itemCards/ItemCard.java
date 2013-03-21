45bf771e5fcde22c8fa599f34a03655f0d5c1029
rodriga <rodriga@rose-hulman.edu>
2013-03-20 22:39:55 -0400

    Added test for ItemCards

---
+++ Betrayal/src/itemCards/ItemCard.java
@@ -0,0 +1,28 @@
+package itemCards;
+
+public class ItemCard {
+	protected String name;
+	protected String description;
+	
+	public void ItemCard(String name, String description){
+		this.name = name;
+		this.description = description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
--- Betrayal/src/test/TestOmenCard.java
+++ Betrayal/src/test/TestOmenCard.java
@@ -15,7 +15,7 @@ public class TestOmenCard {
 	public void CrystalBallOmenInit() {
 		card = new CrystalBall("Crystal Ball", "Hazy images appear in the glass.");
 		assertEquals("Crystal Ball", card.getName());
-		assertEquals("Hazy images appear in the glass", card.getQuote());
+		assertEquals("Hazy images appear in the glass.", card.getQuote());
 	}
 
 }
