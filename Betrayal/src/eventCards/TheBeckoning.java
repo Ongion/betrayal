e25cafaeaa7a4c229e220bf3da88d9035cd69611
Lindsey Watterson <watterlm@rose-hulman.edu>
2013-04-10 21:56:06 -0400

    The remaining 9 event cards for this milestone without methods so I can begin tests

---
+++ Betrayal/src/eventCards/TheBeckoning.java
@@ -0,0 +1,17 @@
+package eventCards;
+
+public class TheBeckoning extends EventCard {
+
+	@Override
+	public void happen(int rollResult) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void happens() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Betrayal/src/eventCards/TheLostOne.java
@@ -0,0 +1,17 @@
+package eventCards;
+
+public class TheLostOne extends EventCard {
+
+	@Override
+	public void happen(int rollResult) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void happens() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
