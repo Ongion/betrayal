f142317ab8ff291663887b0fab9bbde836bef045
rodriga <rodriga@rose-hulman.edu>
2013-04-15 13:09:03 -0400

    Added constructor to GameFrame to pass init tests

--- Betrayal/src/popUps/GameFrame.java
+++ Betrayal/src/popUps/GameFrame.java
@@ -3,6 +3,15 @@ package popUps;
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
+
+	private int width;
+	private int height;
+	
+	public GameFrame(int windowWidth, int windowHeight) {
+		this.width = windowWidth;
+		this.height = windowHeight;
+		
+	}
 	
 
 		
