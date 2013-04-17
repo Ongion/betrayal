68f559d9b96ef301b688365553c05a4b4c98a4d2
Benjamin Paul <paulbi@rose-hulman.edu>
2013-04-17 19:38:10 -0400

    Merging some branches

---
+++ Betrayal/src/popUps/GameFrame.java
@@ -0,0 +1,24 @@
+package popUps;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+	private int width;
+	private int height;
+	
+	public GameFrame(int windowWidth, int windowHeight) {
+		this.width = windowWidth;
+		this.height = windowHeight;
+		
+	}
+
+	public GameFrame() {
+		
+	}
+	
+
+		
+	}
+
+
